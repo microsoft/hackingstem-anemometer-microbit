7b830ff1525a4d912248687992b72d57151c5fe9
damyka <damyka@microsoft.com>
2021-06-17 15:14:10 -0700

    added link to github

--- anemometer.py
+++ anemometer.py
@@ -25,7 +25,7 @@
 #
 #  Comments, contributions, suggestions, bug reports, and feature
 #  requests are welcome! For source code and bug reports see:
-#  http://github.com/[TODO github path to Hacking STEM]
+#  https://github.com/microsoft/hackingstem-anemometer-microbit
 #
 #  Copyright 2018, Jeremy Franklin-Ross
 #  Microsoft EDU Workshop - HackingSTEM
